7db019a3eea871aeb59580960ff09320dec5a212
Sameer2244 <31188576+Sameer2244@users.noreply.github.com>
2023-03-28 21:42:54 +0530

    array-created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [treemap, setTreemap] = useState([
+    { folder1: [] },
+    {
+      folder2: [
+        {
+          folder1: [
+            'file1',
+            'file2',
+            { folder1: [] },
+            'file3',
+            { folder2: [] },
+          ],
+        },
+        { folder1: [] },
+      ],
+    },
+    { folder3: [] },
+  ]);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
